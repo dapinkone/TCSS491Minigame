e1c1656717aeae97e1d4235d1e2a68e67fc6e082
Peter Iriarte <piriar@uw.edu>
2023-01-30 02:12:21 -0800

    added frame

--- main.js
+++ main.js
@@ -23,16 +23,21 @@ ASSET_MANAGER.downloadAll(() => {
 	gameEngine.mainCharacter = new MainCharacter({
 			row: 2,
 			mode: "RUN",
-			location: { x: 0, y: 50 },
+			location: { x: 0, y: 64*2 },
 			scale: 2
 		});
 	
 	gameEngine.init(ctx);
-	buildBackground(2*16, 0*16);	// NOTE: canvas is 64x48 blocks in dimension
+	buildBackground(2*16, 0*16);	// NOTE: canvas is 16x12 blocks in dimension
 	gameEngine.addEntity(gameEngine.example = new Block(0, 0, 2*Block.blockwidth, 3*Block.blockwidth, true, true));
+	// building a frame around the sides:
 	for(const col of Array(16).keys()) { // draw some ground
-		const row = 10;
-		gameEngine.addEntity(new Block(8*16, 2*16, col*Block.blockwidth, row*Block.blockwidth, true, false));
+		gameEngine.addEntity(new Block(8*16, 2*16, col*Block.blockwidth, 0*Block.blockwidth, true, false)); // top row
+		gameEngine.addEntity(new Block(8*16, 2*16, col*Block.blockwidth, 11*Block.blockwidth, true, false)); // bottom row
+	}
+	for(const row of Array(10).keys()) {
+		gameEngine.addEntity(new Block(8*16, 2*16, 0*Block.blockwidth, (row+1)*Block.blockwidth, true, false)); // left col
+		gameEngine.addEntity(new Block(8*16, 2*16, 15*Block.blockwidth, (row+1)*Block.blockwidth, true, false)); // right col
 	}
 	gameEngine.addEntity(gameEngine.mainCharacter);
 	
