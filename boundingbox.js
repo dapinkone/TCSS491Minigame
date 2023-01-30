7fec3a0f1c6d54b15c5ba811c09c47396b5ff8ec
Peter Iriarte <piriar@uw.edu>
2023-01-30 06:14:45 -0800

    additional collision code

--- boundingbox.js
+++ boundingbox.js
@@ -1,19 +1,29 @@
 class BoundingBox {
     constructor({ width, height, location, color = "green" }) {
         Object.assign(this, { width, height, location, color });
+        this.left = location.x;
+        this.top = location.y;
+        this.right = this.left + width;
+        this.bottom = this.top + height;
     }
+    // collision(other) {
+    //     if (other == undefined) return false
+    //     // check if any of the 4 corners exist within the other shape
+    //     if (!other.getCorners) console.log("unknown collision type: ", other.constructor.name)
+    //     const corners = this.getCorners();
+    //     for (const point of Object.keys(corners)) {
+    //         if (other.contains(corners[point])) {
+    //             //console.log("collision ", other.constructor.name, point);
+    //             return true;//point; // conveniently, most strings evalutate to true.
+    //         }
+    //     }
+    //     return false;
+    // }
     collision(other) {
-        if (other == undefined) return false
-        // check if any of the 4 corners exist within the other shape
-        if (!other.getCorners) console.log("unknown collision type: ", other.constructor.name)
-        const corners = this.getCorners();
-        for (const point of Object.keys(corners)) {
-            if (other.contains(corners[point])) {
-                //console.log("collision ", other.constructor.name, point);
-                return true;//point; // conveniently, most strings evalutate to true.
-            }
-        }
-        return false;
+        return (this.right >= other.left // other is on the left?
+            && this.left <= other.right // other is on the right?
+            && this.top <= other.bottom // other is below
+            && this.bottom >= other.top); // other is above
     }
     contains(point) {
         return (
