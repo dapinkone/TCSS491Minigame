2d1fb496ef7ac792e9a986a7d4cc569ee0dddc33
Peter Iriarte <piriar@uw.edu>
2023-02-08 11:48:28 -0800

    added level loading from image template

--- levels.js
+++ levels.js
@@ -20,20 +20,26 @@ const buildBackground = (sxCol, syRow) => {
     }
     ctx.restore();
 }
+let courseMap;
 function loadLevel(levelnum) {
     // clear entities
     gameEngine.clearEntities();
+
+
     const level = levels[levelnum];
     // background
-    buildBackground(...level.background);
-    // runs of blocks ( design is run-length-encoded)
-    for (const run of level.blocks) {
-        runOfBlocks(...run);
+    //buildBackground(...level.background);
+    if (level.levelMap) loadLevelFromMap(level.levelMap);
+    else {
+        // runs of blocks ( design is run-length-encoded)
+        for (const run of level.blocks) {
+            runOfBlocks(...run);
+        }
+        if (level.movers) for (const mover of level.movers) {
+            gameEngine.addEntity(new Mover(...mover));
+        }
+        gameEngine.addEntity(new VictoryBlock(...level.victory));
     }
-    if(level.movers) for(const mover of level.movers) {
-        gameEngine.addEntity(new Mover(...mover));
-    }
-    gameEngine.addEntity(new VictoryBlock(...level.victory));
     // add player back at specified location
     gameEngine.mainCharacter = new MainCharacter({
         row: 2,
@@ -43,6 +49,76 @@ function loadLevel(levelnum) {
     });
     gameEngine.addEntity(new HUD());
     gameEngine.addEntity(gameEngine.mainCharacter);
+
+
+
+
+}
+
+function loadLevelFromMap(levelMap) {
+    // TODO: code cleanup, add "heavy" and "phantom" blocks
+    console.log("loading level from ", levelMap);
+    courseMap = new class {
+        constructor() {
+            this.img = ASSET_MANAGER.getAsset(levelMap);
+            //ctx.drawImage(this.sheet, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+            gameEngine.ctx.drawImage(this.img, 0, 0);
+            this.imgdata = gameEngine.ctx.getImageData(0, 0, this.img.width, this.img.height);
+            this.width = this.img.width;
+            this.height = this.img.height;
+            this.pixels = this.chunk(
+                this.chunk(this.imgdata.data, 4), // chunk data by pixel(rgba values)
+                this.img.width // chunk pixels by row
+            );
+        }
+        chunk(xs, chunkSize) {
+            console.log("chunking ", xs.length, " by ", chunkSize);
+            return [...Array(Math.ceil(xs.length / chunkSize)).keys()]
+                .map(i => xs.slice(i * chunkSize, (i + 1) * chunkSize))
+        }
+        draw() { }
+        update() { }
+    }();
+    const pixelEq = (a, b) => a.length == b.length && [...Array(a.length).keys()]
+        .every(i => a[i] == b[i]);
+
+    courseMap.draw();
+
+    for (let row = 0; row < courseMap.height; row++) {
+        let moverStart;
+        let moverLength = 0;
+        for (let col = 0; col < courseMap.width; col++) {
+            const pixel = courseMap.pixels[row][col];
+            if (pixelEq(pixel, [255, 255, 0, 255])) { // movers are yellow
+                console.log("mover:", `(${col},${row})`)
+                if (moverStart === undefined) {
+                    moverStart = col;
+                } else {
+                    moverLength++;
+                }
+            } else if (moverStart !== undefined) { // witness end of a mover run
+                console.log("mover:", moverStart, moverLength, moverStart + moverLength);
+                gameEngine.addEntity(new Mover(row, moverStart, moverStart + moverLength));
+                moverStart = moverLength = undefined;
+            }
+
+            if (pixelEq(pixel, [0, 0, 0, 255])) { // frame (black)
+                console.log("frame", row, col);
+                runOfBlocks(8, 2, row, col, 1, true, true);
+            }
+            else if (pixelEq(pixel, [255, 0, 0, 255])) { // platform(red)
+                console.log('platform');
+                runOfBlocks(0, 1, row, col, 1, true, true);
+            } else if (pixelEq(pixel, [0, 255, 0, 255])) { // victory block (green)
+                console.log("victoryblock", row, col);
+                gameEngine.addEntity(new VictoryBlock(row, col));
+            } else if (pixelEq(pixel, [0,0,0,0] )) { // transparent is NOP
+                // pass
+            } else {
+                console.log("unknown pixel: ", pixel);
+            }
+        }
+    }
 }
 const frame = (sxCol, syRow) => [
     [sxCol, syRow, 0, 0, 16, true, true, false, false], // building a frame around the sides:
@@ -55,35 +131,36 @@ const levels = {
     current: 0,
     0: {
         background: [8, 0],
-        blocks: [
+        /*blocks: [
             ...frame(8, 2),
             [0, 1, 3, 13, 8, false, true],// vertical bar
             [0, 1, 3, 11, 1, false, true],
             [0, 1, 5, 9, 1, false, true],
             [0, 1, 7, 7, 1, false, true], // floating steps
-            [0, 1, 9, 5, 1, false, true], 
+            [0, 1, 9, 5, 1, false, true],
         ],
 
-        victory: [10, 14],
+        victory: [10, 14],*/
+        levelMap: "./assets/level0.png",
         entryPoint: { x: 64 * 1, y: 64 * 10 }// entrypoint at bottom
     },
     1: {
         background: [6, 0],
         blocks: [
             ...frame(8, 2),
-            
+
             [0, 1, 4, 11, 1, false, true],
             [0, 1, 4, 9, 1, false, true],
-            [0, 1, 4, 7, 1, false, true], 
-            [0, 1, 4, 5, 1, false, true], 
-            [0, 1, 4, 3, 1, false, true], 
-            [0, 1, 5, 1, 1, false, true], 
-            [0, 1, 7, 3, 1, false, true], 
-            [0, 1, 9, 1, 1, false, true], 
+            [0, 1, 4, 7, 1, false, true],
+            [0, 1, 4, 5, 1, false, true],
+            [0, 1, 4, 3, 1, false, true],
+            [0, 1, 5, 1, 1, false, true],
+            [0, 1, 7, 3, 1, false, true],
+            [0, 1, 9, 1, 1, false, true],
 
             [0, 1, 3, 13, 8, false, true], // vertical bar
         ],
-        victory: [10,14],
+        victory: [10, 14],
         entryPoint: { x: 64 * 1, y: 64 * 8 }
     },
     2: {
@@ -99,7 +176,7 @@ const levels = {
             [7, 1, 10],
             [9, 3, 12],
         ],
-        victory: [ 2, 14],
+        victory: [2, 14],
         entryPoint: { x: 64 * 1, y: 64 * 10 }// entrypoint at bottom
     },
 
