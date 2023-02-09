8496cdb4a2da9ce766ae7f3c6c790a2face31214
Peter Iriarte <piriar@uw.edu>
2023-02-08 22:55:18 -0800

    added time to hud

--- hud.js
+++ hud.js
@@ -19,6 +19,12 @@ class HUD {
         ctx.font = "12pt serif";
         const char = gameEngine.mainCharacter;
         const coords =  char.location;
-        ctx.fillText(`${Math.floor(coords.x)}, ${Math.floor(coords.y)} ${char.falling}`, 0, 12);
+        //ctx.fillText(`${Math.floor(coords.x)}, ${Math.floor(coords.y)} ${char.falling}`, 0, 12);
+
+        const gameTime = Math.floor(gameEngine.timer.gameTime);
+        const seconds = String(gameTime % 60).padStart(2, '0');
+        const mins = Math.floor(gameTime / 60);
+        const canvasWidth = gameEngine.ctx.canvas.canvasWidth;
+        ctx.fillText(`${mins}:${seconds}`, 0 , 12);
     }
 }
